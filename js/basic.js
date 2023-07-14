6b5e15584fb4fff605bab009982c828791a2db25
maphouse <maphouse@users.noreply.github.com>
2023-07-14 07:11:59 +0000

    deploy: 8d1c71e7dac486871b101bb70bab05ce2dfe379a

--- js/basic.js
+++ js/basic.js
@@ -1,23 +1,27 @@
 console.log('JS loaded');
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM fully loaded and parsed');
     
-const open = document.getElementById('open');
-const close = document.getElementById('close');
-navitems = document.querySelectorAll('.nav-items > a');
+  const open = document.getElementById('open');
+  const close = document.getElementById('close');
+  navitems = document.querySelectorAll('.nav-items > a');
 
-open.addEventListener('click', function() {
-    navitems.forEach((navitem) => {
-        navitem.style.display = '';
-        navitem.style.opacity = '1';
-    });
-    open.style.display = 'none';
-    close.style.display = 'flex';
-});
+  open.addEventListener('click', function() {
+      navitems.forEach((navitem) => {
+          navitem.style.display = '';
+          navitem.style.opacity = '1';
+      });
+      open.style.display = 'none';
+      close.style.display = 'flex';
+  });
 
-close.addEventListener('click', function() {
+  close.addEventListener('click', function() {
     navitems.forEach((navitem) => {
         navitem.style.display = 'none';
         navitem.style.opacity = '0';
     });
     open.style.display = 'flex';
     close.style.display = 'none';
+  });
+
 });
