0eb337b79808964d126b814fca7891a7397c87b6
maphouse <eehmshaw@gmail.com>
2023-09-12 19:13:42 -0400

    js error test

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,32 @@
 console.log('JS loaded');
 
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('DOM loaded');
-
-    const open = document.getElementById('open');
-    const close = document.getElementById('close');
-    navitems = document.querySelectorAll('.nav-item');
-
-    open.addEventListener('click', function() {
-        navitems.forEach((navitem) => {
-            navitem.style.display = '';
-            navitem.style.opacity = '1';
+try {
+    document.addEventListener('DOMContentLoaded', function() {
+        console.log('DOM loaded');
+    
+        const open = document.getElementById('open');
+        const close = document.getElementById('close');
+        navitems = document.querySelectorAll('.nav-item');
+    
+        open.addEventListener('click', function() {
+            navitems.forEach((navitem) => {
+                navitem.style.display = '';
+                navitem.style.opacity = '1';
+            });
+            open.style.display = 'none';
+            close.style.display = 'flex';
         });
-        open.style.display = 'none';
-        close.style.display = 'flex';
+    
+        close.addEventListener('click', function() {
+        navitems.forEach((navitem) => {
+            navitem.style.display = 'none';
+            navitem.style.opacity = '0';
+        });
+        open.style.display = 'flex';
+        close.style.display = 'none';
+        });
     });
-
-    close.addEventListener('click', function() {
-    navitems.forEach((navitem) => {
-        navitem.style.display = 'none';
-        navitem.style.opacity = '0';
-    });
-    open.style.display = 'flex';
-    close.style.display = 'none';
-    });
-});
+}
+catch (e) {
+    console.log("ERROR: ", e);
+}
