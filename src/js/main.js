41761b2a2b5e3838ab6afb47d9915f75b2a4fcbc
maphouse <eehmshaw@gmail.com>
2023-09-12 22:43:29 -0400

    fix navbar issue

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,27 @@
 console.log('JS loaded');
 
-try {
-    document.addEventListener('DOMContentLoaded', function() {
-        console.log('DOM loaded');
-    
-        const open = document.getElementById('open');
-        const close = document.getElementById('close');
-        navitems = document.querySelectorAll('.nav-item');
-    
-        open.addEventListener('click', function() {
-            navitems.forEach((navitem) => {
-                navitem.style.display = '';
-                navitem.style.opacity = '1';
-            });
-            open.style.display = 'none';
-            close.style.display = 'flex';
-        });
-    
-        close.addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM loaded');
+
+    const open = document.getElementById('open');
+    const close = document.getElementById('close');
+    const navitems = document.querySelectorAll('.nav-item');
+
+    open.addEventListener('click', function() {
         navitems.forEach((navitem) => {
-            navitem.style.display = 'none';
-            navitem.style.opacity = '0';
+            navitem.style.display = '';
+            navitem.style.opacity = '1';
         });
-        open.style.display = 'flex';
-        close.style.display = 'none';
-        });
+        open.style.display = 'none';
+        close.style.display = 'flex';
     });
-}
-catch (e) {
-    console.log("ERROR: ", e);
-}
+
+    close.addEventListener('click', function() {
+    navitems.forEach((navitem) => {
+        navitem.style.display = 'none';
+        navitem.style.opacity = '0';
+    });
+    open.style.display = 'flex';
+    close.style.display = 'none';
+    });
+});
